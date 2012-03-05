a465d55e4b8369cb7ef5a7c8c89b2fa290aeecee
Jonathan Clark <jon.h.clark@gmail.com>
2012-03-05 14:14:31 -0500

    Handle .gz grammars

--- binopt/wrap_input.py
+++ binopt/wrap_input.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
 import codecs
+import os
+import os.path
 from xml.sax.saxutils import escape
 
 graPrefix = sys.argv[1]
@@ -10,5 +12,14 @@ sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
 
 i = 0
 for line in sys.stdin:
-  print '<seg id="%d" grammar="%s%d"> '%(i,graPrefix,i) + escape(line.strip()) + " </seg>"
+  filename = "%s%d"%(graPrefix,i)
+  if not os.path.exists(filename):
+    filenameGz = filename + ".gz"
+    if not os.path.exists(filenameGz):
+      print >>sys.stderr, "Grammar file not found: ", filename, filenameGz
+      sys.exit(1)
+    else:
+      filename = filenameGz
+    
+  print '<seg id="%d" grammar="%s"> '%(i,filename) + escape(line.strip()) + " </seg>"
   i+=1
