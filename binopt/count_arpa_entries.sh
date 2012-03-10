950e7bec4c87d7a08a06bdf752a1c45c0b62b4aa
Jonathan Clark <jon.h.clark@gmail.com>
2012-03-10 17:06:35 -0500

    Hack around SIGPIPE

--- binopt/count_arpa_entries.sh
+++ binopt/count_arpa_entries.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
-set -ueo pipefail
+set -u
+set +eo pipefail # we'll get a SIGPIPE from zcat
 
-count=$(awk -F' |=' '/^ngram/{n+=$3} /\\1-/{print n; exit 0}')
-if [[ $# == 1 && "$1" == "--add-unk" ]]; then
+file=$1
+
+if [ ! -e $file ]; then
+    echo >&2 "ERROR: File not found: $file"
+    exit 1
+fi
+
+count=$(zcat $file | awk -F' |=' '/^ngram/{n+=$3} /\\1-/{print n; exit 0}')
+if [[ $# == 2 && "$2" == "--add-unk" ]]; then
     count=$(($count + 1))
 fi
 echo $count
