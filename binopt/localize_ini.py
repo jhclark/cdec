fa1293df7972f99efc33975a478596a632ccb3c6
Jonathan Clark <jon.h.clark@gmail.com>
2012-04-20 14:13:09 -0400

    Script to aid in making better use of SSDs by copying resources needed by a .ini to a local directory

---
+++ binopt/localize_ini.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import shutil
+import re
+import os.path
+
+localDir = sys.argv[1]
+
+files = dict()
+
+pat = re.compile(r'/[a-zA-Z0-9./_-]+')
+
+# 1) Find paths in the INI that need localizing
+#    and print out new ini
+for line in sys.stdin:
+    for src in pat.findall(line):
+        file = os.path.basename(src)
+        dest = os.path.normpath(localDir + "/" + file)
+        while dest in files.values():
+            dest += ".1"
+        files[src] = dest
+        print >>sys.stderr, "Found {} => {}".format(src, dest)
+        line = line.replace(src, dest)
+    print line,
+
+# 3) Copy files to local destination
+for (src, dest) in files.iteritems():
+    print >>sys.stderr, "Copying {} => {}".format(src, dest)
+    shutil.copy(src, dest)
