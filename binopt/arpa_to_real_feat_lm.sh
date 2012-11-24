568ed0cc922f33ca3770eba12e4cbb089a76eb13
Jonathan Clark <jon.h.clark@gmail.com>
2012-11-24 15:05:47 -0500

    Script to convert ARPA file into feat LM format expected by JLM builder

---
+++ binopt/arpa_to_real_feat_lm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+
+# Converts an ARPA file into a real-valued "feature LM"
+# usable as input to build_binary_jlm (when compiled with -DJLM_REAL_VALUES)
+#
+# Uses STDIN/STDOUT
+
+awk -F$'\t' '/^-/{print $2"\tLanguageModel="$1"\tLanguageModel="$3}'
