06b044a513175540f5d33dcfd40396eb01be0313
Jonathan Clark <jon.h.clark@gmail.com>
2012-11-24 15:08:07 -0500

    Add build flags to allow -DJLM_REAL_VALUES

--- binopt/build.sh
+++ binopt/build.sh
@@ -1,2 +1,7 @@
-g++ -g -I$HOME/prefix/include/ -I/home/jhclark/software/cdec/klm -I/home/jhclark/software/cdec/utils -I/home/jhclark/software/cdec/decoder -c build_binary_jlm.cc
-g++ -g -lz $HOME/software/cdec/klm/util/file.o $HOME/software/cdec/klm/util/mmap.o $HOME/software/cdec/klm/util/exception.o build_binary_jlm.o -o build_binary_jlm
+#!/usr/bin/env bash
+set -ueo pipefail
+set -x
+flags="$@"
+
+g++ -g -I$HOME/prefix/include/ -I/home/jhclark/software/cdec/klm -I/home/jhclark/software/cdec/utils -I/home/jhclark/software/cdec/decoder -c build_binary_jlm.cc $flags
+g++ -g -lz $HOME/software/cdec/klm/util/file.o $HOME/software/cdec/klm/util/mmap.o $HOME/software/cdec/klm/util/exception.o build_binary_jlm.o -o build_binary_jlm $flags
