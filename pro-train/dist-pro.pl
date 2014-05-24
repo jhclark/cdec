02f114d432884794c90ce43f0cab0c2215503217
Jonathan Clark <jon.h.clark@gmail.com>
2014-05-24 17:37:50 -0400

    Bugfix: Extract feature to ID mapping for each PRO iteration *before* running the mappers by just parsing the hypergraphs

--- pro-train/dist-pro.pl
+++ pro-train/dist-pro.pl
@@ -399,7 +399,7 @@ while (1){
 
                 # Before running the mapper, create a mapping from sequential integers to feature names so that we can store
                 # the k-best list in a slightly more compact way
-                $featTableScript = "zcat $dir/hgs/*.json.gz | $FEAT_TABLE > $dir/kbest/kbest.feats.gz";
+                my $featTableScript = "zcat $dir/hgs/*.json.gz | $FEAT_TABLE > $dir/kbest/kbest.feats.gz";
 		check_bash_call($featTableScript);
 
 		my $script = "$MAPPER -s $srcFile -m $metric $refs_comma_sep -w $inweights -K $dir/kbest -k $kbest_size $kbest_hammer_flag < $dir/splag.$im1/$shard > $dir/splag.$im1/$mapoutput";
