827276cebe869a02d27c0675e2af4b0be560ef52
Jonathan Clark <jon.h.clark@gmail.com>
2014-12-06 13:27:44 -0500

    Fix localization of INI

--- binopt/localize_ini.py
+++ binopt/localize_ini.py
@@ -14,18 +14,19 @@ pat = re.compile(r'/[a-zA-Z0-9./_+-]+')
 #    and print out new ini
 for line in sys.stdin:
     for src in pat.findall(line):
-        file = os.path.basename(src)
-        dest = os.path.normpath(localDir + "/" + file)
-        while dest in files.values():
-            dest += ".1"
-        files[src] = dest
-        print >>sys.stderr, "Found {} => {}".format(src, dest)
-        line = line.replace(src, dest)
-        # Special case for JLM meta file
-        (srcMeta, destMeta) = (src + ".meta", dest + ".meta")
-        if os.path.exists(srcMeta):
-            files[srcMeta] = destMeta
-    print line,
+        if src not in files:
+            file = os.path.basename(src)
+            dest = os.path.normpath(localDir + "/" + file)
+            while dest in files.values():
+                dest += ".1"
+            files[src] = dest
+            print >>sys.stderr, "Found {} => {}".format(src, dest)
+            line = line.replace(src, dest)
+            # Special case for JLM meta file
+            (srcMeta, destMeta) = (src + ".meta", dest + ".meta")
+            if os.path.exists(srcMeta):
+                files[srcMeta] = destMeta
+        print line,
 
 # 3) Copy files to local destination
 for (src, dest) in files.iteritems():
